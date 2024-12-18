21d545d4c086c31cf6514e44a08c52277654a875
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-17 22:40:14 -0300

    agenda.jsp

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -9,18 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.DAO;
 
-/**
- * Servlet implementation class Controller
- */
 @WebServlet(urlPatterns = { "/Controller", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	DAO dao = new DAO();
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public Controller() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -32,10 +26,21 @@ public class Controller extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		dao.testeConexao();
+		String action = request.getServletPath();
+		System.out.println(action);
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
 	}
+
+	// Listar Contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+
+	}
+
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -21,16 +21,6 @@ public class DAO {
 			System.out.println(e);
 			return null;
 		}
-	}
-	//teste de conexão
-	public void testeConexao() {
-		try {
-			Connection con = conectar();
-			System.out.println(con);
-			con.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
+	
 	}
 }
