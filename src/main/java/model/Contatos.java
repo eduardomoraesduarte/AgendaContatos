29cef34f0f16e3d58d2071c9fd006ad6156f056f
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-17 21:56:18 -0300

    html e css

---
+++ src/main/java/model/Contatos.java
@@ -0,0 +1,64 @@
+package model;
+
+public class Contatos {
+
+	private Integer id;
+	private String nome;
+	private String email;
+	private String fone;
+	
+	public Contatos() {
+	}
+	
+	
+	public Contatos(Integer id, String nome, String email, String fone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.fone = fone;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getFone() {
+		return fone;
+	}
+
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	
+	
+}
